1f93d1a343aabd78c3451aaa6e72fb2b95740826
Jacob Wan <jacobwan840@gmail.com>
2021-06-13 08:32:03 -0700

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,1 +1,17 @@
-module.exports = {}
+module.exports = {
+  // The default tag format includes a "v" prefix
+  tagFormat: "${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "git add dist",
+      },
+    ],
+    "@semantic-release/git",
+    "@semantic-release/github",
+  ],
+}
